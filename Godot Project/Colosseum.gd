4623ee563c492513ca5f4a413a8a947090a0d97c
TiagoRG99 <tiagoricog99@gmail.com>
2018-11-06 19:03:30 +0000

    Menu-5

---
+++ Godot Project/Colosseum.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_menuButton_pressed():
+	get_tree().change_scene("res://NewGameScreen.tscn")
--- Godot Project/Seller.gd
+++ Godot Project/Seller.gd
@@ -5,3 +5,7 @@ extends Control
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://NewGameScreen.tscn")
+
+func _ready():
+	$Animation_Container.show()
+	$Animation_Container.seller_anim()
