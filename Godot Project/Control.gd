72dc98415ef6bdb5e97bde7a27340a5bbede5dce
RubenWilsonP <ruben-wilson@hotmail.com>
2018-11-16 00:21:33 +0000

    Selectchar_3

--- Godot Project/Control.gd
+++ Godot Project/Control.gd
@@ -2,9 +2,11 @@ extends Control
 
 var heroe_num = 1
 var heroe_count = 3
+const ARQUIVO = "user://save.data"
 
 func _ready():
-	$player.set_texture(load("res://Sprites PI/Selectchar/heroe_1.png"))
+	carregar_dados()
+	$player.set_texture(load("res://Sprites PI/Selectchar/heroe_" + str(heroe_num) + ".png"))
 	pass
 
 func _on_Button_pressed():
@@ -23,7 +25,37 @@ func _on_Button2_pressed():
 	
 func set_player():
 	$player.set_texture(load("res://Sprites PI/Selectchar/heroe_" + str(heroe_num) + ".png"))
+	salvar_dados()
 
 
 func _on_Select_pressed():
 	get_tree().change_scene("res://NewGameScreen.tscn")
+	
+func salvar_dados():
+	var arquivo = File.new()
+	var erro = arquivo.open(ARQUIVO, File.WRITE)
+	
+	var dados = {"heroe value" : heroe_num }
+	
+	if not erro :
+		arquivo.store_var(dados)
+	else:
+		print ("Erro ao salvar dados")
+	
+	arquivo.close()
+	
+func carregar_dados():
+	var arquivo = File.new()
+	var erro = arquivo.open(ARQUIVO, File.READ)
+	
+	
+	if not erro :
+		var dados_salvos = arquivo.get_var()
+		heroe_num = dados_salvos["heroe value"]
+		print (dados_salvos)
+	else:
+		print ("Erro ao salvar dados")
+	
+	arquivo.close()
+	
+	
