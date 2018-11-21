aad425f081795dfa35772f6a128d0938fb12eb14
TiagoRG99 <tiagoricog99@gmail.com>
2018-11-21 20:18:55 +0000

    Colosseum_3

--- Godot Project/Colosseum.gd
+++ Godot Project/Colosseum.gd
@@ -4,3 +4,24 @@ extends Control
 
 func _on_menuButton_pressed():
 	get_tree().change_scene("res://NewGameScreen.tscn")
+
+
+
+
+func _on_Walk_Left_pressed():
+	$TextureRect/Player.animation = "Walk_Elf_1"
+
+
+func _on_Power_Attack_pressed():
+	$TextureRect/Player.animation = "Attack_Elf_1"
+
+
+func _on_Normal_Attack_pressed():
+	$TextureRect/Player.animation = "Attack_Elf_1"
+
+
+func _on_Walk_Right_pressed():
+	$TextureRect/Player.animation = "Walk_Elf_1"
+
+
+
