46027e3228ba22d69ccd1cca50431541e1c5425e
TiagoRG99 <tiagoricog99@gmail.com>
2018-11-22 18:28:39 +0000

    Colosseum_4

--- Godot Project/Colosseum.gd
+++ Godot Project/Colosseum.gd
@@ -3,25 +3,61 @@ extends Control
 
 
 func _on_menuButton_pressed():
-	get_tree().change_scene("res://NewGameScreen.tscn")
+	#get_tree().change_scene("res://NewGameScreen.tscn")
+	startTurn()
 
 
+
+func _ready():
+	$TextureRect/Enemy.health = 100
+	$TextureRect/Player.health = 100
+
+
+func endTurn():
+	$TextureRect/TextureRect/Walk_Right.disabled = true
+	$TextureRect/TextureRect/Walk_Left.disabled = true
+	$TextureRect/TextureRect/Normal_Attack.disabled = true
+	$TextureRect/TextureRect/Power_Attack.disabled = true
+
+func startTurn():
+	$TextureRect/TextureRect/Walk_Right.disabled = false
+	$TextureRect/TextureRect/Walk_Left.disabled = false
+	$TextureRect/TextureRect/Normal_Attack.disabled = false
+	$TextureRect/TextureRect/Power_Attack.disabled = false
 
 
 func _on_Walk_Left_pressed():
 	$TextureRect/Player.animation = "Walk_Elf_1"
+	if not $TextureRect/Player.position.x < 100:
+		$TextureRect/Player.position.x = $TextureRect/Player.position.x - 20
+	endTurn()
 
 
 func _on_Power_Attack_pressed():
-	$TextureRect/Player.animation = "Attack_Elf_1"
+	$TextureRect/Player.animation = "AttackPower_Elf_1"
+	$TextureRect/Player.position.x = $TextureRect/Player.position.x + 5
+	if $TextureRect/Player.position.x - $TextureRect/Enemy.position.x < 30:
+		$TextureRect/Enemy.health = $TextureRect/Enemy.health - 50
+	endTurn()
 
 
 func _on_Normal_Attack_pressed():
-	$TextureRect/Player.animation = "Attack_Elf_1"
+	$TextureRect/Player.animation = "AttackNormal_Elf_1"
+	$TextureRect/Player.position.x = $TextureRect/Player.position.x + 5
+	if $TextureRect/Player.position.x - $TextureRect/Enemy.position.x < 30:
+		$TextureRect/Enemy.health = $TextureRect/Enemy.health - 20
+	endTurn()
+
 
 
 func _on_Walk_Right_pressed():
 	$TextureRect/Player.animation = "Walk_Elf_1"
+	if not $TextureRect/Enemy.position.x - $TextureRect/Player.position.x < 35:
+		$TextureRect/Player.position.x = $TextureRect/Player.position.x + 20
+	endTurn()
 
 
 
+func _on_Player_animation_finished():
+	if $TextureRect/Player.animation != "Idle_Elf_1":
+		$TextureRect/Player.animation = "Idle_Elf_1"
--- Godot Project/Enemy.gd
+++ Godot Project/Enemy.gd
@@ -1,15 +1,13 @@
 extends AnimatedSprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var health = 100
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if health <= 0:
+		print ("dead")
+	pass
